332ed6a67b9723f1521dfecb371a12f685153570
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-04-01 17:33:42 +0000

    debut api finance

--- extract/tweets_api.py
+++ extract/tweets_api.py
@@ -11,27 +11,14 @@ class ExtractTweets :
         self.__ACCES_SECRET = fetch_config.ACCES_SECRET
         self.__CONSUMER_KEY = fetch_config.CONSUMER_KEY
         self.__CONSUMER_SECRET = fetch_config.CONSUMER_SECRET
-        
-        print('*' *50) 
-        print(type(self.__ACCES_KEY))
-        print('*' *50) 
+        self.__auth  = None
 
 
     def authentification(self) -> None:   
 
         # Twitter authentication
-        auth = tweepy.OAuthHandler( self.__ACCES_KEY, self.__ACCES_SECRET)   
-        auth.set_access_token(self.__CONSUMER_KEY, self.__CONSUMER_SECRET) 
+        self.__auth = tweepy.OAuthHandler( self.__ACCES_KEY, self.__ACCES_SECRET)   
+        self.__auth.set_access_token(self.__CONSUMER_KEY, self.__CONSUMER_SECRET) 
                
-        # Creating an API object 
-        api = tweepy.API(auth)
-        
-        # get user tweets 
-        tweets = api.user_timeline(screen_name='@'+str('elonmusk'), 
-                                count=50,
-                                include_rts = False,
-                                tweet_mode = 'extended') 
-        print("Authentification successfull")
-        
-
-ExtractTweets().authentification()
+        return self.__auth 
+    
